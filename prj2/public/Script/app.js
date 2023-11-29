f458d663721525e0a6d30936da6fa25fa4aad548
Mahsa Dorri <mahsadorri093@gmail.com>
2023-11-28 21:34:03 -0500

    Amanda Ikea and Mahsa Dorri Part for COMP231

---
+++ prj2/public/Script/app.js
@@ -0,0 +1,9 @@
+//IIFe-- Immediately invoked function expression
+(function(){
+    function start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", start);
+})();
